42925aadc76c90460fec82b262021cc8c2405bad
LWM <lilmouse@littlewhitemouse.net>
2020-02-06 09:45:56 -0500

    Can save even if stack of chunks is outside DS

--- DeepStorage/Deep_Storage_SaveLoad.cs
+++ DeepStorage/Deep_Storage_SaveLoad.cs
@@ -16,23 +16,28 @@ namespace LWM.DeepStorage
     // Saving:
     //   Problem: stone chunks and slag are saved "compressed," 'cause
     //   they are all basically the same.  But...if there are more than
-    //   one in a DeepStorage unit, then they shouldn't be compressed.
+    //   one in a cell, then they shouldn't be compressed.
+    //
+    //   We cannot simply check for a DSU - there might just be a giant
+    //   pile on the ground (maybe DSU got moved/destroyed).  (Alternate
+    //   option: make sure anything compressible explodes all over on
+    //   DSU despawning.)
     //
     //
     // Loading:
     //   Problem: when loading, the game will try to load all the things
     //    in the Deep Storage unit and then will realize it cannot place
-    //    them because there is already something there (see Spawn() in 
-    //    Deep_Storage_PutInto.cs)  So things could end up exploded all 
+    //    them because there is already something there (see Spawn() in
+    //    Deep_Storage_PutInto.cs)  So things could end up exploded all
     //    over the place.  Not pretty.
     //   Why?  Because items get loaded AFTER buildings.  Well, crap.
     //   Bonus problem:  Maybe there's a stack of objects on the ground
     //    because a DSU got destroyed; we want to keep those intact, too.
     //
     //   Solution:
-    //    Spawn() does know if it's respawningAfterLoad.  When we 
+    //    Spawn() does know if it's respawningAfterLoad.  When we
     //    patch it in Deep_Storage_PutInto.cs, we use that as a
-    //    check to avoid the problem.  No additional patching 
+    //    check to avoid the problem.  No additional patching
     //    required here.
     //
     //**********************************************************//
@@ -41,14 +46,13 @@ namespace LWM.DeepStorage
         static void Postfix(ref bool __result, Thing t)
         {
             if (__result == false) return;
-            SlotGroup slotGroup = t.Position.GetSlotGroup(t.Map);
-            if (slotGroup == null || !(slotGroup?.parent is ThingWithComps) ||
-                (slotGroup.parent as ThingWithComps).TryGetComp<CompDeepStorage>() == null)
-            {
-                return;
+            if (!t.Spawned) return;
+            var ots=t.Map.thingGrid.ThingsListAt(t.Position);
+            int items=0;
+            foreach (var ot in ots) {
+                if (ot.def.EverHaulable) items++;
             }
-            // Hey, we're in deep storage!
-            __result = false;
+            if (items>1) __result=false;
             return;
         }
     }
