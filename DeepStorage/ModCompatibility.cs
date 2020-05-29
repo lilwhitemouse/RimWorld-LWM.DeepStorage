2b156ec426c0e4e2c28b59743e4bac0bfa9552db
lilwhitemouse <lilmouse@littlewhitemouse.net>
2020-05-28 23:03:37 -0400

    Remove fix now included in base game

--- DeepStorage/ModCompatibility.cs
+++ DeepStorage/ModCompatibility.cs
@@ -61,66 +61,6 @@ namespace LWM.DeepStorage
         }
     }
 
-    /* A way to head off problems - this showed up with Avil's Common Sense
-     *  See https://github.com/catgirlfighter/RimWorld_CommonSense/blob/master/Source/CommonSense/CommonSense/WorkGiver_Merge_Patch.cs
-     * If a different mod changes how CanStackWith works, the merging code will put pawns in a loop where they
-     * will think they can merge things in Deep Storage (because merging code looks at the defs) and then when
-     * the put it into storage, they don't stack, so there's extra lying around, so the pawn picks it up because
-     * they think it can be merged (because....) &c &c &c.
-     * In vanilla this works because there can only be 1 stack in a cell, so the HaulToCell job fails (because
-     *   they cannot stack), so the problem gets short circuited.
-     * Not so in Deep Storage, where the job gets created and the loop started...
-     */
-    /* I originally created this patch for Common Sense, and Avil adopted the patch there.  However, on
-     * further reflection....I should have it here, too...perhaps even more than there.  Rather than remove
-     * it there, I'll add it here and make sure it doesn't double-patch.
-     * Note: yes, this should be here: any time there are objects that have the same def, but are not
-     * stackable b/c of whatever reason, this will be a potential issue - especially if someone plays
-     * with one of the stack-adjusting mods */
-    // Note: Ludeon is aware of this bug and may even fix it.
-    [HarmonyPatch(typeof(RimWorld.WorkGiver_Merge), "JobOnThing")]
-    static class Fix_Vanilla_WorkGiver_Merge {
-        // Replace WorkGiver_Merge's JobOnThing's test of "if ... (thing.def==t.def) ..." with
-        //                                                "if ... (thing.CanStackWith(t)) ..."
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            var code = new List<CodeInstruction>(instructions);
-            var i = 0; // using two for loops
-            while (i < code.Count) {
-                FieldInfo defFI=typeof(Verse.Thing).GetField("def");
-                yield return code[i];
-                i++;
-                if (code.Count-i>5 && // We may not find this if someone else has patched WorkGiver_Merge
-                    code[i].opcode == OpCodes.Ldfld &&
-                    (FieldInfo)code[i].operand == defFI &&
-                    code[i + 2]?.opcode == OpCodes.Ldfld &&
-                    (FieldInfo)code[i + 2].operand == defFI &&
-                    code[i + 3].opcode == OpCodes.Bne_Un ) {//OpCodes.Beq) {
-                    Log.Message("LWM.DeepStorage: fixing vanilla merge bug/compatibility with CommonSense.");
-                    // Found it!
-                    // Instead of loading the two defs and checking if they are equal,
-                    i++;  // advance past the .def call
-                    yield return code[i]; // put the second thing on the stack
-                    i = i + 2; // advance past the 2nd thing(we just added it) and its .def call
-                    // Call thing.CanStackWith(t);
-                    yield return new CodeInstruction(OpCodes.Callvirt, typeof(Thing).GetMethod("CanStackWith"));
-
-                    // i now points to branch not equal (unsigned) //1.0 was "branch if equal"
-                    CodeInstruction c = new CodeInstruction(OpCodes.Brfalse);// CodeInstruction(OpCodes.Brtrue);
-                    c.operand = code[i].operand; // grab its target
-                    yield return c;
-                    i++; // advance past bne
-                    // continue returning everything:
-                    break;
-                }
-            } // end first for loop
-            for (; i < code.Count; i++)
-            {
-                yield return code[i];
-            }
-        } // end Transpler
-    } // end fix for WorkGiver_Merge's JobOnThing
-
     /* A cheap cute way to post a Message, for when an xml patch operation is done :p */
     public class PatchMessage : PatchOperation {
         protected override bool ApplyWorker(System.Xml.XmlDocument xml) {
