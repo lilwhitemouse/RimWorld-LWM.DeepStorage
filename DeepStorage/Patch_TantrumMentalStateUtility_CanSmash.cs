38f5654089f467a15e4d30357ac325b7c6aa716b
lilwhitemouse <lilmouse@littlewhitemouse.net>
2021-04-28 01:24:23 -0400

    Basic isSecure setting - make it harder for tantrums to destroy valuables

--- DeepStorage/Patch_TantrumMentalStateUtility_CanSmash.cs
+++ DeepStorage/Patch_TantrumMentalStateUtility_CanSmash.cs
@@ -3,22 +3,41 @@ using HarmonyLib;
 using RimWorld;
 using Verse;
 using Verse.AI;
-
+/*************
+ * Limit damage a pawn can do while having a tantrum:
+ *   Only allow pawn to check "top" item in Deep Storage
+ *   Only allow any item half the time (because dealing 
+ *     with tightly packed storage is a pain)
+ *   Mostly disallow any item in a "safe" storage unit
+ *     (e.g., the Safe) - only 1/10 chance
+ *************/
 namespace LWM.DeepStorage
 {
 	[HarmonyPatch(typeof(TantrumMentalStateUtility), "CanSmash")]
-	internal class Patch_TantrumMentalStateUtility_CanSmash
+	static class Patch_TantrumMentalStateUtility_CanSmash
 	{
-		// Token: 0x06000154 RID: 340 RVA: 0x0000BFA8 File Offset: 0x0000A1A8
 		[HarmonyPostfix]
 		private static void AfterCanSmash(Pawn pawn, Thing thing, ref bool __result)
 		{
-			if (__result && (!(thing is ThingWithComps) || thing.TryGetComp<CompDeepStorage>() == null))
+			if (__result && (thing.TryGetComp<CompDeepStorage>() == null))
 			{
-				SlotGroup slotGroup = thing.Position.GetSlotGroup(pawn.Map);
-				ThingWithComps thingWithComps = ((slotGroup != null) ? slotGroup.parent : null) as ThingWithComps;
-				CompDeepStorage compDeepStorage = (thingWithComps != null) ? thingWithComps.GetComp<CompDeepStorage>() : null;
-				__result = (compDeepStorage == null);
+                // It's in deep storage
+                if (thing.Position.GetSlotGroup(pawn.Map)?.parent is ThingWithComps thingWithComps && // null is not TwC
+                    thingWithComps.GetComp<CompDeepStorage>() is CompDeepStorage cds) {
+                    // if it's not on top and easily accessible to angry colonist,
+                    if (!Utils.TopThingInDeepStorage.Contains(thing) 
+                        // (or only 50% chance of picking something in storage anyway),
+                    || thing.thingIDNumber %2 == 1) {
+                        // don't break it:
+                        __result = false;
+                        return;
+                    }
+                    if (cds.cdsProps.isSecure && !(thing.thingIDNumber % 10 == 0)) { // subject to review?
+                        __result = false;
+                        return;
+                    }
+                }
+                // otherwise, it's a valid target to get smashed
 			}
 		}
 	}
