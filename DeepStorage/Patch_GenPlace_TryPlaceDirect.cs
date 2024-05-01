41f357dcf4424206acde2082dc7e95f521f3f0ce
LWM <lilmouse@littlewhitemouse.net>
2024-05-01 13:15:55 -0400

    Fix bug where cache doesn't clear if last item in unit isn't full stack but is filled

---
+++ DeepStorage/Patch_GenPlace_TryPlaceDirect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using System.Linq;
+using HarmonyLib;
+using System.Reflection;
+using System.Reflection.Emit; // for OpCodes in Harmony Transpiler
+using UnityEngine;
+
+using static LWM.DeepStorage.Utils.DBF;
+
+namespace LWM.DeepStorage
+{
+    [HarmonyPatch(typeof(Verse.GenPlace), "TryPlaceDirect")]
+    static class Patch_GenPlace_TryPlaceDirect
+    {
+        static void Postfix(Map map, IntVec3 loc)
+        {
+            Utils.Mess(Utils.DBF.TryPlaceDirect, "LWM.DS: clearing cache at " + loc + " after TryPlaceDirect");
+            map.GetComponent<MapComponentDS>().DirtyCache(loc);
+        }
+    }
+}
