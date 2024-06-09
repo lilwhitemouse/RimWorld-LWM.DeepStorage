04ebf75752e4a7558c1df4b90d11225920208fd5
LWM <lilmouse@littlewhitemouse.net>
2024-06-09 13:02:35 -0400

    Hack fix for assemblies being copied over until I figure out WTF

--- _Mod/prepare-releases.sh
+++ _Mod/prepare-releases.sh
@@ -6,17 +6,19 @@ then
 fi
 
 cd ../DeepStorage
-#....fuuuuuuuuuuck, msbuild command line is for whatever reason copying over the package references, and
+#....fuuuuuuuuuuck, building is for whatever reason copying over the package references, and
 # we can't have that. So fuck this whole thing anyway
-#msbuild
+msbuild
 cd ../_Mod
+rm LWM.DeepStorage/1.5/Assemblies/0*
+rm LWM.DeepStorage/1.5/Assemblies/H*
 zip -r LWM-DeepStorage-Debug.$1.zip LWM.DeepStorage
 cd ../DeepStorage
-#msbuild '/p:Configuration=Release'
+msbuild '/p:Configuration=Release'
 cd ../_Mod
+rm LWM.DeepStorage/1.5/Assemblies/0*
+rm LWM.DeepStorage/1.5/Assemblies/H*
 zip -r LWM-DeepStorage.$1.zip LWM.DeepStorage
 
-echo "NOTE: IF YOU DID NOT BUILD THIS IN MONODEVELOP YOU HAVE PROBLEMS!"
-
-
+echo "CHECK ASSEMBLIES!!"
 
