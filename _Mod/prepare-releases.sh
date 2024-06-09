da3667f73b8df9aaa7abd6895f7c5f8ac5ee4a6a
LWM <lilmouse@littlewhitemouse.net>
2024-06-09 12:53:49 -0400

    Prepare for 1.5.0.4

--- _Mod/prepare-releases.sh
+++ _Mod/prepare-releases.sh
@@ -6,13 +6,17 @@ then
 fi
 
 cd ../DeepStorage
-msbuild
+#....fuuuuuuuuuuck, msbuild command line is for whatever reason copying over the package references, and
+# we can't have that. So fuck this whole thing anyway
+#msbuild
 cd ../_Mod
 zip -r LWM-DeepStorage-Debug.$1.zip LWM.DeepStorage
 cd ../DeepStorage
-msbuild '/p:Configuration=Release'
+#msbuild '/p:Configuration=Release'
 cd ../_Mod
 zip -r LWM-DeepStorage.$1.zip LWM.DeepStorage
+
+echo "NOTE: IF YOU DID NOT BUILD THIS IN MONODEVELOP YOU HAVE PROBLEMS!"
 
 
 
